e2574368bae9d9b7f27c1b027717561956ff59b6
furuya <furuya.to@uhmfg.co.jp>
2022-06-23 23:05:21 +0900

    initial commit

--- hello.go
+++ hello.go
@@ -6,12 +6,17 @@ import (
 )
 
 func main(){
+	// 新しいアプリケーションを作成
 	a := app.New()
 
+	// ウィンドウを作成
 	w := a.NewWindow("Hello")
+	// 表示コンテンツの設定
 	w.SetContent(
+		// Hello Fyne!という内容でラベルを作成
 		widget.NewLabel("Hello Fyne!"),
 	)
 
+	//ウィンドウを表示し実行
 	w.ShowAndRun()
 }
