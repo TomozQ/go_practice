e1cc2b96eb8fbdb8e0844c8371e30bc37bf1e979
furuya <furuya.to@uhmfg.co.jp>
2022-06-24 15:20:51 +0900

    multiple parts

--- hello.go
+++ hello.go
@@ -3,20 +3,42 @@ package main
 import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
+	"strconv"
 )
 
 func main(){
-	// 新しいアプリケーションを作成
 	a := app.New()
-
-	// ウィンドウを作成
 	w := a.NewWindow("Hello")
-	// 表示コンテンツの設定
+	l := widget.NewLabel("Hello Fyne!")
+	e := widget.NewEntry()
+	e.SetText("0")
 	w.SetContent(
-		// Hello Fyne!という内容でラベルを作成
-		widget.NewLabel("Hello Fyne!"),
+		// 縦に一列にラベルを表示する
+		container.NewVBox(
+		// 横一列にラベルを表示する
+		// container.NewHBox(
+			l, e,
+			widget.NewButton("Click me!", func(){
+				// eから入力値を受け取る
+				n, _ := strconv.Atoi(e.Text)
+				// labelに設定されたテキストを変更する。
+				l.SetText("Total: " + strconv.Itoa(total(n)))
+			}),
+		),
 	)
 
-	//ウィンドウを表示し実行
+	a.Settings().SetTheme(theme.DarkTheme())
 	w.ShowAndRun()
+}
+
+func total(n int) int {
+	
+	t := 0
+	for i := 1; i <= n; i++{
+		t += i
+	}
+
+	return t
 }
