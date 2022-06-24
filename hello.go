d7aecabead9d5e21f57b2ff01c7db09dcf4e545f
furuya <furuya.to@uhmfg.co.jp>
2022-06-24 15:31:05 +0900

    checkbox

--- hello.go
+++ hello.go
@@ -4,41 +4,26 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/theme"
-	"strconv"
 )
 
 func main(){
 	a := app.New()
 	w := a.NewWindow("Hello")
 	l := widget.NewLabel("Hello Fyne!")
-	e := widget.NewEntry()
-	e.SetText("0")
+	c := widget.NewCheck("Check!", func(f bool){	//チェックが変更された際に実行される変数
+		if f {
+			l.SetText("Checked!")
+		}else {
+			l.SetText("Not Checked")
+		}
+	})
+	// チェック状態を変更
+	c.SetChecked(true)
 	w.SetContent(
-		// 縦に一列にラベルを表示する
 		container.NewVBox(
-		// 横一列にラベルを表示する
-		// container.NewHBox(
-			l, e,
-			widget.NewButton("Click me!", func(){
-				// eから入力値を受け取る
-				n, _ := strconv.Atoi(e.Text)
-				// labelに設定されたテキストを変更する。
-				l.SetText("Total: " + strconv.Itoa(total(n)))
-			}),
+			l, c,
 		),
 	)
 
-	a.Settings().SetTheme(theme.DarkTheme())
 	w.ShowAndRun()
-}
-
-func total(n int) int {
-	
-	t := 0
-	for i := 1; i <= n; i++{
-		t += i
-	}
-
-	return t
 }
