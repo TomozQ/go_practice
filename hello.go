9c18932940414a75e43a9a9ae9f9fdd442391893
furuya <furuya.to@uhmfg.co.jp>
2022-06-26 18:34:43 +0900

    process json

--- hello.go
+++ hello.go
@@ -1,70 +1,56 @@
-// Webスクレーピング
+// JSONデータ処理
 
 package main
 
 import (
-	"github.com/PuerkitoBio/goquery"
+	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	
 )
 
-// Webスクレーピング...取得したコンテンツから有用な情報を探して取り出して処理する（Webデータ抽出）
-// goquery...HTMLをパース処理し、必要な情報を簡単に取り出せるようにしてくれるパッケージ
-
-// Documentの作成
-// HTMLから<<Document>>という構造体を作成
-
-// URLアドレスから作成
-// 変数 := goquery.NewDocument( アドレス )
-
-// Responseから作成
-// 変数 := goquery.NewDocumentFromResponse( <<Response>> )
-
-// Readerから作成
-// 変数 := goquery.NewDocumentFromReader( <<Reader>> )
-
-// Findによる検索処理
-// Find...ドキュメントないにあるHTML要素を検索するためのもの
-// 変数 := <<Document>>.Find( 検索対象 )
-// 引数...検索対象をstringで指定
-// 戻り値...「Selection」という構造体
-
-// Selectionからテキストと属性を取得
-// テキストを取得
-// 変数 := <<Selection>>.Text()
-
-// 要素にある属性の値を取得
-// 変数1, 変数2 := <<Selection>>.Attr( 名前 )
-// 問題が発生した場合には変数2にerrorが格納される
-
-// 要素のHTMLを取得
-// 変数1, 変数2 := <<Selection>>.Html()
-// 戻り値...変数1 -> string, 変数2 -> error
-
-// 要素の前後の要素を取得
-// 変数 := <<Selection>>.BeforeSelection()
-// 変数 := <<Selection>>.AfterSelection()
-
-// 要素の親要素を取得
-// 変数 := <<Selection>>.Parent()
-
-// 要素に組み込まれている子要素を取得
-// 変数 := <<Selection>>.Children()
-
-// aタグのリンクを出力する
+// FirebaseからJSONデータを取得する
 func main(){
-	p := "https://golang.org"
-	doc, er := goquery.NewDocument(p) // goqueryではhttp.GetのようにCloseでの開放を考える必要がない。
+	p := "https://tuyano-dummy-data.firebaseio.com/mydata.json"
+	// reにResponseが格納される。
+	re, er := http.Get(p)
+
+	if er != nil {
+		panic(er)
+	}
+	defer re.Body.Close()
+	
+	// re.Bodyから全てのコンテンツを変数に格納
+	s, er := ioutil.ReadAll(re.Body)
+	if er != nil {
+		panic(er)
+	}
+	
+	// 型を指定した変数を用意
+	var data []interface{}
+	
+	er = json.Unmarshal(s, &data) // 第二引数にdataのポインタを渡す
+	// -> 変数sに入れてあるJSONデータがGoの[]interface{}型の値に変換されdataに代入される。
+
+	fmt.Println(data)
+	// [
+	// 		map[mail:zero@zero name:taro tel:000-000] 
+	// 		map[mail:syoda@tuyano.com name:tuyano tel:999-999] 
+	// 		map[mail:hanako@flower name:hanako tel:888-888] 
+	// 		map[mail:sachiko@happy name:sachiko tel:777-777]
+	// ]
 
 	if er != nil {
 		panic(er)
 	}
 
-	// Findの引数にcssセレクタでaタグを指定
-	doc.Find("a").Each(func(n int, sel *goquery.Selection) {
-	// Eachの引数 -> func ( <<int>>, <<Selection>> ){....}
-	// 第一引数には、取得したSelectionのindexがint値で渡される。第二引数には取り出したSelectionが渡される。
-		
-		// href属性の値を取り出す
-		lk, _ := sel.Attr("href")
-		println(n, sel.Text(), "(", lk, ")")
-	})
+	// dataにループを回す
+	for i, im := range(data){ // dataから各データの値が変数imに取り出される。
+		// 型アサーション ... 空のインターフェイス型の値を特定の方の値として取り出し直すのに使う機能
+		// 変数 := 値.( 型 ) -> map[string]interface{} => キーをstring型、値をinterface型で指定。
+		m := im.(	map[string]interface{} ) // 指定した型として値が変数に取り出される。
+		fmt.Println(i, m["name"].(string), m["mail"].(string), m["tel"].(string)) // 出力も 値.( 型 ) としてstring型に変換している。
+	}
+
 }
