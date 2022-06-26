b41e10cdc41455e1e81dcce58c3ef7012ff6c504
furuya <furuya.to@uhmfg.co.jp>
2022-06-26 15:18:26 +0900

    file access open/write/close

--- hello.go
+++ hello.go
@@ -1,39 +1,63 @@
 package main
 
 import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
-	"fyne.io/fyne/v2/container"
+	"os"
+	"fmt"
+	"hello"
 )
 
+// OpenFie
+// 変数 := os.OpenFile(ファイルパス, フラグ, <FileMode>)
+
 func main(){
-	a := app.New()
-	w := a.NewWindow("Hello")
-	l := widget.NewLabel("Hello Fyne!")
-	//  入力フォーム
-	ne := widget.NewEntry()
-	// パスワード入力フォーム
-	pe := widget.NewPasswordEntry()
+	// 値書き出し関数
+	wt := func(f *os.File, s string){ // f *os.FileにOpenFileで開いたファイルの構造体が渡される s strignが書き出すテキスト
+		// 引数に指定されたstringをファイルに書きだす。
+		_, er := f.WriteString(s + "\n")
 
-	w.SetContent(
-		container.NewVBox(
-			l, 
-			// フォームを作成
-			widget.NewForm(
-				// フォーム内に部品を置いていく
-				widget.NewFormItem("Name", ne), // 入力フォーム
-				widget.NewFormItem("Pass", pe), // パスワード入力フォーム
-			),
-			// ボタン配置
-			widget.NewButton("OK", func(){
-				// ne.Text() -> neの入力値
-				l.SetText(ne.Text + " & " + pe.Text)
-			}),
-		),
-	)
+		// エラーがあった場合にはエラーを出力してファイルを閉じる
+		if er != nil {
+			fmt.Println(er)
+			f.Close()
+			return
+		}
+	}
 
-	w.ShowAndRun()
+	// 書き出すファイル名を指定
+	fn := "data.txt"
+	// ファイル名にfnを指定
+	// O_APPEND...既にある場合には最後に追記する
+	// O_CREATE...ファイルがない場合には新たに作成
+	// O_WRONLY...書き出し専用モード
+	f, er := os.OpenFile(fn, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	// エラーがあった場合にはエラーを出力してファイルを閉じる
+	if er != nil {
+		fmt.Println(er)
+		return
+	}
+
+	fmt.Println("***start***")
+	// wt関数にOpenFileで開いたファイルの構造体とstringを渡す
+	wt(f, "***start***")
+
+	for {
+		// helloパッケージのInput関数を実行
+		s := hello.Input("type message")
+		// 空白でエンターが入力されたら処理を終了する
+		if s == "" {
+			break
+		}
+		// 開いているファイルと入力されたテキストを渡す
+		wt(f, s)
+	}
+
+	// 上のforがbreakされたらendをファイルに書き出し、コンソールに出力する
+	wt(f, "*** end ***\n\n")
+	fmt.Println("*** end ***")
+	// ファイルを閉じる
+	er = f.Close()
+	// エラーが出た場合にはエラーを出力する。
+	if er != nil {
+		fmt.Println(er)
+	}
 }
-
-// Formはwebとは認識が違う。
-// FyneではあくまでデザインのためにFormの構造体を利用する。
