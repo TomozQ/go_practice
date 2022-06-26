8734f083964a667d38d17e871c376c629ebb8e33
furuya <furuya.to@uhmfg.co.jp>
2022-06-26 15:41:35 +0900

    file read

--- hello.go
+++ hello.go
@@ -1,35 +1,35 @@
 package main
 
 import (
+	"io/ioutil"
 	"os"
 	"fmt"
-	"hello"
 )
 
 // OpenFie
 // 変数 := os.OpenFile(ファイルパス, フラグ, <FileMode>)
-// panic( <<error>> )
-// defer ...実行する処理...
+// panic( <<error>> ) -> プログラムを強制終了する
+// defer ...実行する処理... -> プログラムを終了する際、最後に必ず実行する処理
 
 func main(){
-	// 値書き出し関数
-	wt := func(f *os.File, s string){ // f *os.FileにOpenFileで開いたファイルの構造体が渡される s strignが書き出すテキスト
-		// 引数に指定されたstringをファイルに書きだす。
-		_, er := f.WriteString(s + "\n")
+	// 値読み込み関数
+	rt := func(f *os.File){ // f *os.FileにOpenFileで開いたファイルの構造体が渡される
+		// 読み取り値をsに格納する
+		s, er := ioutil.ReadAll(f) // ReadAllで全てをまとめて取り出す。戻り値はbyte配列とerror
 
 		// エラーがあった場合にはエラーを出力してファイルを閉じる
 		if er != nil {
 			panic(er)
 		}
+
+		fmt.Println(string(s))
 	}
 
 	// 書き出すファイル名を指定
 	fn := "data.txt"
 	// ファイル名にfnを指定
-	// O_APPEND...既にある場合には最後に追記する
-	// O_CREATE...ファイルがない場合には新たに作成
-	// O_WRONLY...書き出し専用モード
-	f, er := os.OpenFile(fn, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	// O_RDONRY...読み出し専用
+	f, er := os.OpenFile(fn, os.O_RDONLY, os.ModePerm)
 	// エラーがあった場合にはエラーを出力してファイルを閉じる
 	if er != nil {
 		panic(er)
@@ -38,28 +38,7 @@ func main(){
 	// defer close
 	defer f.Close()
 
-	fmt.Println("***start***")
-	// wt関数にOpenFileで開いたファイルの構造体とstringを渡す
-	wt(f, "***start***")
-
-	for {
-		// helloパッケージのInput関数を実行
-		s := hello.Input("type message")
-		// 空白でエンターが入力されたら処理を終了する
-		if s == "" {
-			break
-		}
-		// 開いているファイルと入力されたテキストを渡す
-		wt(f, s)
-	}
-
-	// 上のforがbreakされたらendをファイルに書き出し、コンソールに出力する
-	wt(f, "*** end ***\n\n")
-	fmt.Println("*** end ***")
-	// ファイルを閉じる
-	er = f.Close()
-	// エラーが出た場合にはエラーを出力する。
-	if er != nil {
-		fmt.Println(er)
-	}
+	fmt.Println("<<start>>")
+	rt(f)
+	fmt.Println("<< end >>")
 }
